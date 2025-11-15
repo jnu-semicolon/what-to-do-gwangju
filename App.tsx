c480da07eab8559f43909f07e0f51a599a68289a
kimminseo <t01053714524@gmail.com>
2025-11-15 22:31:23 +0900

    commit : 홈화면 버그 수정

--- App.tsx
+++ App.tsx
@@ -1,8 +1,7 @@
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View, SafeAreaView } from 'react-native';
+import { StatusBar, StyleSheet, useColorScheme } from 'react-native';
 import {
   SafeAreaProvider,
-  useSafeAreaInsets,
+  SafeAreaView,
 } from 'react-native-safe-area-context';
 import HomeScreen from './src/pages/HomePage/HomeScreen';
 
@@ -18,16 +17,6 @@ function App() {
 }
 
 function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
 
   return(
     <SafeAreaView style={styles.container}>
